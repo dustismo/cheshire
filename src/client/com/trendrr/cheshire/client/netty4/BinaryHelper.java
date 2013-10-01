c56dfae6a4a92b2238af9816231ea1e6351e7b0a
Dustin Norlander <dnorlander@twitter.com>
2013-10-01 15:01:54 -0700

    add string32 helpers, throw exception on too large string

--- src/client/com/trendrr/cheshire/client/netty4/BinaryHelper.java
+++ src/client/com/trendrr/cheshire/client/netty4/BinaryHelper.java
@@ -5,6 +5,8 @@ package com.trendrr.cheshire.client.netty4;
 
 import java.io.UnsupportedEncodingException;
 
+import com.trendrr.oss.exceptions.TrendrrException;
+import com.trendrr.oss.exceptions.TrendrrOverflowException;
 import io.netty.buffer.ByteBuf;
 
 import org.apache.commons.logging.Log;
@@ -19,8 +21,22 @@ import org.apache.commons.logging.LogFactory;
 public class BinaryHelper {
 
 	protected static Log log = LogFactory.getLog(BinaryHelper.class);
-	
-	public static void writeString(ByteBuf out, String str) {
+
+    public static void writeString32(ByteBuf out, String str) {
+        if (str == null) {
+            out.writeInt(0);
+            return;
+        }
+        try {
+            byte[] bytes = str.getBytes("utf8");
+            out.writeInt(bytes.length);
+            out.writeBytes(bytes);
+        } catch (UnsupportedEncodingException e) {
+            out.writeShort((short)0);
+            log.error("Caught", e);
+        }
+    }
+	public static void writeString(ByteBuf out, String str) throws TrendrrException {
 		if (str == null) {
 			out.writeShort((short)0);
 			return;
@@ -28,11 +44,14 @@ public class BinaryHelper {
 		
 		try {
 			byte[] bytes = str.getBytes("utf8");
+            if (bytes.length > Short.MAX_VALUE) {
+                throw new TrendrrOverflowException("String too long!");
+            }
+
 			out.writeShort((short)bytes.length);
 			out.writeBytes(bytes);
 		} catch (UnsupportedEncodingException e) {
-			out.writeShort((short)0);
-			log.error("Caught", e);
+			throw new TrendrrException("Bad string encoding!", e);
 		}
 	}
 }
